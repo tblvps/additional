90589e110991b2083935cbe0ef8045145d26c77d
tblvps <tblvps@gmail.com>
2024-11-29 23:30:02 +0600

    Add .env to .gitignore

--- machine/settings.py
+++ machine/settings.py
@@ -28,6 +28,13 @@ LOGIN_URL = '/admin/login/'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'shahinmondal42bd@gmail.com'
+EMAIL_HOST_PASSWORD = 'fsyy spny fwbh qmjy'
+
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
